c3549a3d801b65b65786e69c6cb30ae4b35a4a78
Alessian Dormal <alessian@alessian.be>
2025-01-22 09:56:40 +0100

    improve seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,59 +8,57 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+USER_COUNT = 100
+CAR_COUNT = rand(50..100)
+BOOKING_COUNT = rand(50..100)
 
-puts "Cleaning the DB"
+puts "Cleaning the database..."
 Booking.destroy_all
-
-puts "Creating Booking DB seed"
-Booking.create(confirmed_booking: false, start_date: "2025-01-10", end_date: "2025-01-20", user_id: 1)
-Booking.create(confirmed_booking: false, start_date: "2025-01-08", end_date: "2025-01-10", user_id: 2)
-Booking.create(confirmed_booking: false, start_date: "2024-12-15", end_date: "2024-12-16", user_id: 1)
-Booking.create(confirmed_booking: false, start_date: "2024-12-05", end_date: "2024-12-12", user_id: 2)
-Booking.create(confirmed_booking: false, start_date: "2024-11-21", end_date: "2024-11-24", user_id: 1)
-
-puts "Seed DB created !"
-
-# require 'faker'
-# Fake Cars
-# 10.times do
-#   car = Car.new ("#{Faker::street_name}, #{rand(1000)}, #{Faker::Address.city},
-#               #{rand(10000)}, #{Faker::Adress.country}",
-#     address:
-#     brand: Faker::Vehicle.manufacturer,
-#     category: Faker::Vehicle.car_type,
-#     model: Faker::Vehicle.model(make_of_model: :brand),
-#     price_per_hour: rand(100)
-#   )
-#   car.save
-# end
-
-# Clean the database
-puts 'cleaning the database...'
 Car.destroy_all
 User.destroy_all
-puts 'database cleaned!'
+puts 'Database cleaned!'
 
-# Create users
-users = 10.times.map do |i|
+puts "Creating users..."
+users = USER_COUNT.times do
   User.create!(
-    email: "user#{i + 1}@example.com",
-    password: "password",
-    first_name: "First#{i + 1}",
-    last_name: "Last#{i + 1}"
+    email: Faker::Internet.unique.email,
+    password: Faker::Internet.password,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
   )
 end
 
-# Create cars
-10.times do |i|
+puts "Creating cars..."
+cars = CAR_COUNT.times do
   Car.create!(
-    address: "Street #{i + 1}, #{i + 10}, City #{i + 1}, 12345, Country",
-    brand: ["Toyota", "Honda", "Ford", "Chevrolet", "Tesla"].sample,
-    category: ["SUV", "Sedan", "Truck", "Coupe", "Hatchback"].sample,
-    model: "Model #{('A'..'Z').to_a.sample}",
+    address: "#{Faker::Address.street_name}, #{rand(1000)}, #{Faker::Address.city},
+              #{rand(10000)}, #{Faker::Address.country}",
+    brand: Faker::Vehicle.manufacturer,
+    category: Faker::Vehicle.car_type,
+    model: Faker::Vehicle.model(make_of_model: :brand),
     price_per_hour: rand(20..100), # Random price between 20 and 100
     user: users.sample # Assign a random user
   )
 end
 
-puts "Seeding complete! Created #{User.count} users and #{Car.count} cars."
+puts "Creating bookings..."
+BOOKING_COUNT.times do
+  booking_user = users.sample
+  available_cars = cars.reject { |car| car.user_id == booking_user.id } # Exclude cars owned by the booking user
+
+  if available_cars.empty?
+    raise "No cars available for booking that satisfy the condition!"
+  end
+
+  booking_car = available_cars.sample
+
+  Booking.create!(
+    confirmed_booking: false,
+    start_date: Faker::Date.forward(days: rand(1..300)),
+    end_date: Faker::Date.between(from: :start_date + 1, to: :start_date + rand(1..7)),
+    user: users.sample,
+    cars: booking_car
+  )
+end
+
+puts "Seeding complete! Created #{User.count} users, #{Car.count} cars and #{Booking.count} bookings."
