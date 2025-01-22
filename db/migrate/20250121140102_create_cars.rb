e74f6b03db0fe16401976d05bed9703e0ef5c242
Alessian Dormal <alessian@alessian.be>
2025-01-22 17:41:08 +0100

    fix bugs linke to car_id/cars_id mismatch

--- db/migrate/20250121140102_create_cars.rb
+++ db/migrate/20250121140102_create_cars.rb
@@ -7,6 +7,7 @@ class CreateCars < ActiveRecord::Migration[7.1]
       t.string :model
       t.integer :price_per_hour
       t.references :user, null: false, foreign_key: true
+      t.references :car, null: false, foreign_key: true
 
       t.timestamps
     end
