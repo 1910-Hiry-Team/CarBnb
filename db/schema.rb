b2dc84fc329341d4de70b9f5d4db60d42dffa85c
Alessian Dormal <alessian@alessian.be>
2025-01-23 16:25:33 +0100

    Misc fixes (#33)
    * fix the current_user in Car#index bug
    
    * changed how the path works
    
    * changed the paths and fixed part of the views
    
    * fix the button in car#index
    
    * fix the overall pathing
    
    ---------
    
    Co-authored-by: Nicolas Beernaert <nicobeernaert@hotmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -48,8 +48,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_23_112047) do
     t.string "end_date"
     t.bigint "user_id", null: false
     t.bigint "car_id", null: false
+    t.bigint "car_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_bookings_on_car_id"
     t.index ["car_id"], name: "index_bookings_on_car_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
