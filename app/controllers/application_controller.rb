1ad67f1c409410351f3d8986ff64c1aa30f2d639
Joachim Clodic <114757577+Jo8467@users.noreply.github.com>
2025-01-24 11:55:25 +0100

    Pundit, New/Create Car, Edit/Update car, Destroy car (#46)
    * Pundit, New/Create Car, Edit/Update car, Destroy car
    
    * minor fix
    
    * minor fix
    
    * fixed user login logout
    
    * fixed user login logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,25 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :store_user_location!, if: :storable_location?
+  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token, if: :devise_controller?
+  include Pundit::Authorization
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+
+  # Pundit: allow-list approach
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  # Uncomment when you *really understand* Pundit!
+  # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  # def user_not_authorized
+  #   flash[:alert] = "You are not authorized to perform this action."
+  #   redirect_to(root_path)
+  # end
+
+
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -16,6 +35,9 @@ class ApplicationController < ActionController::Base
   end
 
   private
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
 
     # Store the last URL if it is a GET request and not a Devise route
   def store_user_location!
